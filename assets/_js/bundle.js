fbd8a74419511663ec528e9cd2463532238d77f2
Jacque Schrag <jnschrag@gmail.com>
2018-11-09 15:51:14 -0500

    Make main menu accessible

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -1,5 +1,7 @@
+import Navigation from './header-nav'
 import objectFitImages from 'object-fit-images'
 
 window.addEventListener('DOMContentLoaded', () => {
+  Navigation()
   objectFitImages()
 })
---
+++ assets/_js/header-nav.js
@@ -0,0 +1,18 @@
+const Navigation = () => {
+  const toggle = document.querySelector('.site-header__nav-trigger')
+  const menu = document.querySelector('.site-header__nav-menu')
+
+  toggle.addEventListener('click', function() {
+    if (menu.classList.contains('is-active')) {
+      this.setAttribute('aria-expanded', 'false')
+      this.classList.remove('is-active')
+      menu.classList.remove('is-active')
+    } else {
+      menu.classList.add('is-active')
+      this.setAttribute('aria-expanded', 'true')
+      this.classList.add('is-active')
+    }
+  })
+}
+
+export default Navigation
