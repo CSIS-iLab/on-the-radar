db6089fa9ade1867df8e78e7dea75533529d3a2d
Jacque Schrag <jnschrag@gmail.com>
2018-11-12 14:40:22 -0500

    Add JS to switch between minimal & full headers

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -1,8 +1,10 @@
+import Header from './header'
 import HeaderSearch from './header-search'
 import Navigation from './header-nav'
 import objectFitImages from 'object-fit-images'
 
 window.addEventListener('DOMContentLoaded', () => {
+  Header()
   HeaderSearch()
   Navigation()
   objectFitImages()
