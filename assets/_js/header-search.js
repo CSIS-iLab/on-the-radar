a8c0e05676cb66cecefcd2768a48a373127602c6
Jacque Schrag <jnschrag@gmail.com>
2018-11-12 09:01:13 -0500

    Style search input bar

--- assets/_js/header-search.js
+++ assets/_js/header-search.js
@@ -1,21 +1,40 @@
 const HeaderSearch = () => {
-  const triggers = document.querySelectorAll('.js-search-trigger')
+  const trigger = document.querySelector('.js-search-trigger')
+  const closeTrigger = document.querySelector('.js-search-trigger-close')
   const form = document.querySelector('.site-header__search-form')
+  const input = document.getElementById('header-search')
 
-  triggers.forEach(trigger => {
-    console.log(trigger)
-    trigger.addEventListener('click', function() {
-      if (form.classList.contains('is-active')) {
-        this.setAttribute('aria-expanded', 'false')
-        this.classList.remove('is-active')
-        form.classList.remove('is-active')
-      } else {
-        form.classList.add('is-active')
-        this.setAttribute('aria-expanded', 'true')
-        this.classList.add('is-active')
-      }
-    })
+  trigger.addEventListener('keydown', function(e) {
+    if (e.key == 'Enter') {
+      e.preventDefault()
+      openSearch()
+    }
   })
+
+  trigger.addEventListener('click', function() {
+    if (form.classList.contains('is-active')) {
+      closeSearch()
+    } else {
+      openSearch()
+    }
+  })
+
+  closeTrigger.addEventListener('click', function() {
+    closeSearch()
+  })
+
+  function openSearch() {
+    form.classList.add('is-active')
+    trigger.setAttribute('aria-expanded', 'true')
+    trigger.classList.add('is-active')
+    input.focus()
+  }
+
+  function closeSearch() {
+    trigger.setAttribute('aria-expanded', 'false')
+    trigger.classList.remove('is-active')
+    form.classList.remove('is-active')
+  }
 }
 
 export default HeaderSearch
