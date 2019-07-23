85da6bc8068e68febce76d77db6221c9e4d84465
Jacque Schrag <jnschrag@gmail.com>
2019-07-23 17:21:37 -0400

    Add divider animation

--- assets/_js/home.js
+++ assets/_js/home.js
@@ -1,7 +1,9 @@
+import Divider from './home-divider'
 import ParallaxBg from './home-parallax'
 import Timeline from './home-timeline'
 
 window.addEventListener('DOMContentLoaded', () => {
+  Divider()
   ParallaxBg()
   Timeline()
 })
