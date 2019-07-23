1c308bbf818f3b767ae0dcacde731e3d4a206c97
Jacque Schrag <jnschrag@gmail.com>
2019-07-23 16:33:06 -0400

    Add parallax bg script

---
+++ assets/_js/home-parallax.js
@@ -0,0 +1,47 @@
+const ParallaxBg = () => {
+  const speed = 0.08
+  const backgrounds = Array.from(document.querySelectorAll('.js-parallax'))
+  let lastScrollY = 0
+  let ticking = false
+
+  function onScroll() {
+    lastScrollY = window.scrollY
+    requestTick()
+  }
+
+  function requestTick() {
+    if (!ticking) {
+      requestAnimationFrame(update)
+      ticking = true
+    }
+  }
+
+  function update() {
+    backgrounds.forEach(el => {
+      const offsetTop = el.offsetTop
+      let limit = offsetTop + el.offsetHeight
+
+      if (lastScrollY > offsetTop && lastScrollY <= limit) {
+        el.style.setProperty(
+          '--scrollparallax',
+          (window.pageYOffset - el.offsetTop) * speed
+        )
+      } else {
+        if (lastScrollY > offsetTop) {
+          el.style.setProperty(
+            '--scrollparallax',
+            (window.pageYOffset - el.offsetTop) * speed
+          )
+        } else {
+          el.style.transform = ''
+        }
+      }
+    })
+
+    ticking = false
+  }
+
+  window.addEventListener('scroll', onScroll, false)
+}
+
+export default ParallaxBg
--- assets/_js/home.js
+++ assets/_js/home.js
@@ -1,5 +1,7 @@
+import ParallaxBg from './home-parallax'
 import Timeline from './home-timeline'
 
 window.addEventListener('DOMContentLoaded', () => {
+  ParallaxBg()
   Timeline()
 })
