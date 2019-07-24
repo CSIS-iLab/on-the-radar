1cf92fcd236bf8d5d0e2fe60df51ffb5b01c7cab
Jacque Schrag <jnschrag@gmail.com>
2019-07-24 10:10:38 -0400

    Add project navigation animation

--- assets/_js/home.js
+++ assets/_js/home.js
@@ -1,9 +1,11 @@
 import Divider from './home-divider'
 import ParallaxBg from './home-parallax'
+import ProjNav from './home-proj-nav'
 import Timeline from './home-timeline'
 
 window.addEventListener('DOMContentLoaded', () => {
   Divider()
   ParallaxBg()
+  ProjNav()
   Timeline()
 })
