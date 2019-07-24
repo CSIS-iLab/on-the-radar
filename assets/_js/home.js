31c33e52354b6c0bca392852a0e27a52f25c7d12
Jacque Schrag <jnschrag@gmail.com>
2019-07-24 12:31:01 -0400

    Add ToC to home page

--- assets/_js/home.js
+++ assets/_js/home.js
@@ -2,10 +2,12 @@ import Divider from './home-divider'
 import ParallaxBg from './home-parallax'
 import ProjNav from './home-proj-nav'
 import Timeline from './home-timeline'
+import TOC from './home-toc'
 
 window.addEventListener('DOMContentLoaded', () => {
   Divider()
   ParallaxBg()
   ProjNav()
   Timeline()
+  TOC()
 })
