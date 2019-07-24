b55b38ce1af695acd82fc9b22ccca4ec54bbfc19
Tucker Harris <tucker.harris121@gmail.com>
2019-07-24 11:09:17 -0400

    add js file for definitions

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -2,6 +2,7 @@ import Header from './header'
 import HeaderSearch from './header-search'
 import Navigation from './header-nav'
 import TippyHome from './home-tippy'
+import Definitions from './home-definitions'
 import objectFitImages from 'object-fit-images'
 
 window.addEventListener('DOMContentLoaded', () => {
