3ef1e6655f775f81ce62bbe786dae0aa76d9fef1
Jacque Schrag <jnschrag@gmail.com>
2019-07-24 12:54:29 -0400

    Move definition & tippy JS to home.js

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -1,15 +1,11 @@
 import Header from './header'
 import HeaderSearch from './header-search'
 import Navigation from './header-nav'
-import TippyHome from './home-tippy'
-import Definitions from './home-definitions'
 import objectFitImages from 'object-fit-images'
 
 window.addEventListener('DOMContentLoaded', () => {
   Header()
   HeaderSearch()
-  TippyHome()
   Navigation()
-  Definitions()
   objectFitImages()
 })
