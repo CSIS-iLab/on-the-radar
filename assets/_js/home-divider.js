325cfe11504293971ac85e3790f215b780bc91fa
Jacque Schrag <jnschrag@gmail.com>
2019-07-29 11:12:41 -0400

    Add check for Intersection Observer & display content if IO isn't supported

--- assets/_js/home-divider.js
+++ assets/_js/home-divider.js
@@ -2,19 +2,27 @@ const Divider = () => {
   const section = document.querySelector('.matter__divider')
   const brackets = document.querySelectorAll('#bracket path')
 
-  let observer = new IntersectionObserver(entry => {
-    if (entry[0].intersectionRatio > 0) {
-      brackets.forEach(bracket => bracket.classList.add('js-animate'))
-    } else {
-      brackets.forEach(bracket => bracket.classList.remove('js-animate'))
-    }
-  })
-
-  observer.observe(section)
-
   brackets.forEach(bracket => {
     bracket.style.setProperty('--length', bracket.getTotalLength())
   })
+
+  if (
+    !('IntersectionObserver' in window) ||
+    !('IntersectionObserverEntry' in window) ||
+    !('intersectionRatio' in window.IntersectionObserverEntry.prototype)
+  ) {
+    brackets.forEach(bracket => bracket.classList.add('js-animate'))
+  } else {
+    let observer = new IntersectionObserver(entry => {
+      if (entry[0].intersectionRatio > 0) {
+        brackets.forEach(bracket => bracket.classList.add('js-animate'))
+      } else {
+        brackets.forEach(bracket => bracket.classList.remove('js-animate'))
+      }
+    })
+
+    observer.observe(section)
+  }
 }
 
 export default Divider
--- assets/_js/home-proj-nav.js
+++ assets/_js/home-proj-nav.js
@@ -1,18 +1,26 @@
 const ProjNav = () => {
   const els = document.querySelectorAll('.js-fade-in')
 
-  let observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.intersectionRatio > 0) {
-        entry.target.classList.add('js-is-visible')
-        observer.unobserve(entry.target)
-      }
+  if (
+    !('IntersectionObserver' in window) ||
+    !('IntersectionObserverEntry' in window) ||
+    !('intersectionRatio' in window.IntersectionObserverEntry.prototype)
+  ) {
+    els.forEach(el => el.classList.add('js-is-visible'))
+  } else {
+    let observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          entry.target.classList.add('js-is-visible')
+          observer.unobserve(entry.target)
+        }
+      })
     })
-  })
 
-  els.forEach(el => {
-    observer.observe(el)
-  })
+    els.forEach(el => {
+      observer.observe(el)
+    })
+  }
 }
 
 export default ProjNav
